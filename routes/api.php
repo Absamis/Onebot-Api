80e8927189bcd8cb5fa448826d503ab8025a9c74
hibee <ibrahimsobande191@gmail.com>
2024-07-07 09:00:31 +0100

    files merged

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,23 @@ use App\Http\Controllers\Configurations\AccountOptionController;
 use App\Http\Controllers\Configurations\RoleController;
 use App\Http\Controllers\Configurations\SigninOptionController;
 use App\Http\Controllers\ConfigurationsController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::prefix("user")->group(function () {
+        Route::get("", [UserController::class, "getUserDetails"]);
+        Route::post("change-profile-photo", [UserController::class, "changeProfilePhoto"]);
+        Route::post("change-pin", [UserController::class, "changePin"]);
+        Route::post("change-password", [UserController::class, "changePassword"]);
+        Route::post("change-tag", [UserController::class, "changeUserTag"]);
+        Route::post("validate-pin", [UserController::class, "validatePin"]);
+        Route::get("referrals", [UserController::class, "getReferrals"]);
+        Route::post("reset-pin", [UserController::class, "resetPin"]);
+        Route::post("update-daily-limit", [UserController::class, "updateDailyLimit"])->middleware("auth.pin");;
+    });
+});
 
 Route::prefix("auth")->group(function () {
     Route::get("{option}/signup-request", [SignupController::class, "signupRequest"]);
